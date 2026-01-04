1fcdf8aa1984b8362aade2e76b1c0eb42eecd1ce
nonxedy <pos18411@gmail.com>
2026-01-04 04:01:09 +0300

    feat: add EntityDeathListener and update Mythic-Dist to 5.9.5

--- src/main/java/org/antagon/acore/Acore.java
+++ src/main/java/org/antagon/acore/Acore.java
@@ -10,6 +10,7 @@ import org.antagon.acore.core.ConfigManager;
 import org.antagon.acore.listener.AdvancementListener;
 import org.antagon.acore.listener.BannerHeadListener;
 import org.antagon.acore.listener.BlockInteractionListener;
+import org.antagon.acore.listener.EntityDeathListener;
 import org.antagon.acore.listener.FogPotionListener;
 import org.antagon.acore.listener.ItemFrameListener;
 import org.antagon.acore.listener.MinecartSpeedListener;
@@ -111,6 +112,10 @@ public final class Acore extends JavaPlugin {
         // Register AdvancementListener
         getServer().getPluginManager().registerEvents(new AdvancementListener(), this);
         getLogger().info("Advancement listener enabled");
+
+        // Register EntityDeathListener
+        getServer().getPluginManager().registerEvents(new EntityDeathListener(), this);
+        getLogger().info("Entity death listener enabled");
     }
 
     private void registerCommands() {
